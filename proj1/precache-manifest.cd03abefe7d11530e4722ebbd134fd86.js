691ffd7ce96dbbee27f52f1cf8a65e216eb69edf
CelanMatjaz <47082652+CelanMatjaz@users.noreply.github.com>
2019-03-26 11:08:10 +0100

    Add files via upload

---
+++ proj1/precache-manifest.cd03abefe7d11530e4722ebbd134fd86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4c04cdaece108433c1a4",
+    "url": "/static/js/main.5a7bfeb3.chunk.js"
+  },
+  {
+    "revision": "3712bca2139b1f386f9b",
+    "url": "/static/js/2.ef2eab03.chunk.js"
+  },
+  {
+    "revision": "4c04cdaece108433c1a4",
+    "url": "/static/css/main.e0e24dce.chunk.css"
+  },
+  {
+    "revision": "4df1dc9aed7bfc5ea29e02dd4e8d2be6",
+    "url": "/index.html"
+  }
+];
---
+++ proj4/precache-manifest.4aa56fa83bf474a5839f4870940d6bb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "797cb7eec8cecce68470",
+    "url": "/static/js/main.a1517381.chunk.js"
+  },
+  {
+    "revision": "caf83310bc2fce2eb813",
+    "url": "/static/js/2.8198b6f7.chunk.js"
+  },
+  {
+    "revision": "797cb7eec8cecce68470",
+    "url": "/static/css/main.cee875dd.chunk.css"
+  },
+  {
+    "revision": "f879bd1421839b2011e342a88af49d07",
+    "url": "/index.html"
+  }
+];
